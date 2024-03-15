814dfd313c83f980cb6b02629ad47a0c216e8793
Snejin <81646732+Snejin@users.noreply.github.com>
2024-03-15 12:24:07 +0300

    Add files via upload

---
+++ normalprog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main(int argc, char** argv) {
+
+printf("sizeof int8_t: %lu\n", sizeof(int8_t)); // char
+printf ("sizeof uint8_t: %lu\n", sizeof(uint8_t)); //unsigned char
+printf ("sizeof int16_t: %lu\n", sizeof(int16_t)); //int
+printf ("sizeof uint16_t: %lu\n", sizeof(uint16_t)); //unsigned int
+printf ("sizeof int16_t: %lu\n", sizeof(int16_t)); //short
+printf ("sizeof int16_t: %lu\n", sizeof(int16_t)); //short int
+printf ("sizeof uint16_t: %lu\n", sizeof(uint16_t)); //short unsigned
+printf ("sizeof uint16_t: %lu\n", sizeof(uint16_t)); //short int unsigned
+printf ("sizeof int16_t: %lu\n", sizeof(int16_t)); //int
+printf ("sizeof uint16_t: %lu\n", sizeof(uint16_t)); //int unsigned
+printf ("sizeof int32_t: %lu\n", sizeof(int32_t)); //long
+printf ("sizeof int32_t: %lu\n", sizeof(int32_t)); //long int
+printf ("sizeof uint32_t: %lu\n", sizeof(uint32_t)); //long unsigned
+printf ("sizeof uint32_t: %lu\n", sizeof(uint32_t)); //long int unsigned
+printf ("sizeof int64_t: %lu\n", sizeof(int64_t)); //long long
+printf ("sizeof int64_t: %lu\n", sizeof(int64_t)); //long long int
+printf ("sizeof uint64_t: %lu\n", sizeof(uint64_t)); //unsigned long 
+printf ("sizeof float: %lu\n", sizeof(float)); //float
+printf ("sizeof double: %lu\n", sizeof(double)); //double
+printf ("sizeof double: %lu\n", sizeof(double)); //long double
+
+
+return 0;
+}
+
